c00b1c96131c405880f12f38103832219550a7fd
ionantha Sun <mr.s.szw@gmail.com>
2020-08-26 15:09:45 +0800

    fix

--- models/download.js
+++ models/download.js
@@ -3,12 +3,12 @@ const axios = require('axios')
 const getFile = {
   async download(access_token, media_id) {
     try {
-      let res = await axios.get('http://file.api.weixin.qq.com/cgi-bin/media/get', {
-        params: {
-          access_token,
-          media_id,
+      let res = await axios(
+        {
+          url: `http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=${access_token}&media_id=${media_id}`,
+          responseType: 'arraybuffer'
         }
-      })
+      )
       console.log(res.data)
       return res.data
     } catch (error) {
--- routes/file.js
+++ routes/file.js
@@ -13,32 +13,8 @@ router.get("/file_upload", async (req, res) => {
   console.log(access_token)
   const data = await download(access_token, media_id)
   console.log(data)
+  fs.writeFileSync(`./public/zhouhp/${media_id}.amr`, data, 'binary')
   res.send(data);
-  /* 生成multiparty对象，并配置上传目标路径 */
-  // let form = new multiparty.Form();
-  // /* 设置编辑 */
-  // form.encoding = 'utf-8';
-  // //设置文件存储路劲
-  // form.uploadDir = './public/amr';
-  // //设置文件大小限制
-  // // form.maxFilesSize = 1 * 1024 * 1024;
-  // form.parse(req, function (err, fields, files) {
-  //   try {
-  //     let inputFile = files.file[0];
-  //     let uploadedPath = inputFile.path;
-  //     let newPath = form.uploadDir + "/" + inputFile.originalFilename;
-  //     //同步重命名文件名 fs.renameSync(oldPath, newPath)
-  //     fs.renameSync(inputFile.path, newPath);
-  //     res.send({ data: "上传成功！" });
-  //     //读取数据后 删除文件
-  //     // fs.unlink(newPath, function () {
-  //     //   console.log("删除上传文件");
-  //     // })
-  //   } catch (err) {
-  //     console.log(err);
-  //     res.send({ err: "上传失败！" });
-  //   };
-  // })
 })
 
 
