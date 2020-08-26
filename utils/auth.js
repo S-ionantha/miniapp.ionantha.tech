466cde5b3ab9a674006314bdca0010a7d403b3db
ionantha Sun <mr.s.szw@gmail.com>
2020-08-26 11:27:27 +0800

    add

--- utils/auth.js
+++ utils/auth.js
@@ -11,7 +11,7 @@ const auth = {
 	getAuthorizeURL (backurl) {
 		const redirectUrl = `https://${process.env.DOMAIN}/api/snsapiUserInfo?backurl=${encodeURIComponent(backurl)}`
 		const state = '1'
-		const scope = 'snsapi_userinfo'
+		const scope = 'snsapi_base'
 		return `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${redirectUrl}&response_type=code&scope=${scope}&state=${state}#wechat_redirect`
 		//return client.getAuthorizeURL(redirectUrl, state, scope)
 	},
