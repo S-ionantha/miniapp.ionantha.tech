2905f28ab1d39ce7b857a3ea4fbe7de1c3de0884
ionantha Sun <mr.s.szw@gmail.com>
2020-08-26 17:24:53 +0800

    add

--- routes/file.js
+++ routes/file.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
-var fs = require("fs");
+let express = require('express');
+let router = express.Router();
+let fs = require("fs");
+const WebSocket = require('ws');
 const { download } = require('../models/download')
 const { getToken } = require('../utils/wechat')
-var amrToMp3 = require('amrToMp3')
+let amrToMp3 = require('amrToMp3')
+const CryptoJS = require('crypto-js')
+
 
 router.get("/file_upload", async (req, res) => {
   const { media_id } = req.query
@@ -12,17 +15,84 @@ router.get("/file_upload", async (req, res) => {
   const data = await download(access_token, media_id)
   console.log(data)
   fs.writeFileSync(`./public/zhouhp/amr/${media_id}.amr`, data, 'binary')
-  // amrToMp3(sourcePath[,outputPath])
-  let error, back
   amrToMp3(`./public/zhouhp/amr/${media_id}.amr`, [`./public/zhouhp/mp3`])
-    .then((a) => {
-      back = a // ./src/mp3/test.mp3
-    })
-    .catch((err) => {
-      error = err
-    })
-  res.send({ error, back});
+  res.send({});
 })
 
+router.get('/', async (req, res) => {
+  let url = 'wss://ise-api.xfyun.cn/v2/open-ise'
+  let host = 'ise-api.xfyun.cn'
+  let apiKey = '8fe1b02cb2b1daeef62114fe0672f1dc'
+  let apiSecret = '4d1149480a8fcc088d603c671e55119c'
+  let date = new Date().toGMTString()
+  let algorithm = 'hmac-sha256'
+  let headers = 'host date request-line'
+  let signatureOrigin = `host: ${host}\ndate: ${date}\nGET /v2/open-ise HTTP/1.1`
+  let signatureSha = CryptoJS.HmacSHA256(signatureOrigin, apiSecret)
+  let signature = CryptoJS.enc.Base64.stringify(signatureSha)
+  let authorizationOrigin = `api_key="${apiKey}", algorithm="${algorithm}", headers="${headers}", signature="${signature}"`
+  let authorization = Buffer.from(authorizationOrigin).toString('base64') 
+  url = `${url}?authorization=${authorization}&date=${date}&host=${host}`
+
+  let webSocket = new WebSocket(url)
+  webSocket.onopen = e => {
+    console.log('open')
+    // this.setStatus('ing')
+    // 重新开始录音
+    // setTimeout(() => {
+    //   this.webSocketSend()
+    // }, 500)
+  }
+  webSocket.onmessage = e => {
+    this.result(e.data)
+  }
+  webSocket.onerror = e => {
+    this.recorderStop()
+  }
+  webSocket.onclose = e => {
+    this.recorderStop()
+  }
+  var params = {
+    common: {
+      app_id: "5f437bc4",
+    },
+    business: {
+      category: 'read_sentence', // read_syllable/单字朗读，汉语专有 read_word/词语朗读  read_sentence/句子朗读 http://test.www.xfyun.cn/doc/Ise/IseAPI.html#%E6%8E%A5%E5%8F%A3%E8%B0%83%E7%94%A8%E6%B5%81%E7%A8%8B
+      rstcd: 'utf8',
+      group: 'pupil',
+      sub: 'ise',
+      ent: 'cn_vip',
+      tte: 'utf-8',
+      cmd: 'ssb',
+      auf: 'audio/L16;rate=16000',
+      aus: 1,
+      aue: 'lame:mp3',
+      text: '\uFEFF' + '今天天气怎么样'
+    },
+    data: {
+      status: 0,
+      // encoding: 'raw',
+      // data_type: 1,
+      data: ''
+    },
+  }
+  webSocket.send(JSON.stringify(params))
+  // webSocket.send(
+  //   JSON.stringify({
+  //     business: {
+  //       cmd: 'auw',
+  //       aus: 2,
+  //       aue: 'raw'
+  //     },
+  //     data: {
+  //       status: 1,
+  //       encoding: 'raw',
+  //       data_type: 1,
+  //       data: this.toBase64(audioData),
+  //     },
+  //   })
+  // )
+  res.send(url);
+})
 
 module.exports = router;
